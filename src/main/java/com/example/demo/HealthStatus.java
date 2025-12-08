6591e4c3dc39e0270ef5415e189430778929d218
ByteCrafter-rgb <ayushwork983@gmail.com>
2025-12-08 20:36:02 +0530

    Added echo controllers

---
+++ src/main/java/com/example/demo/HealthStatus.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class HealthStatus {
+    private final String echo;
+
+    public HealthStatus(String echo) {
+        this.echo = echo;
+    }
+
+    public String getEcho() {
+        return echo;
+    }
+}
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 // 1. @RestController marks this class as capable of handling incoming web requests
@@ -12,5 +14,10 @@ public class HelloController {
     @GetMapping("/")
     public String sayHello() {
         return "Hello World! This is my first Spring Boot API.";
+    }
+
+    @PostMapping("/echo")
+    public HealthStatus checkHealth(@RequestBody HealthStatus inputStatus) {
+        return inputStatus;
     }
 }
